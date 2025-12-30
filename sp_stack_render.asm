611e296442ef5e9e7f0dc589fd3a93a6216fdc1c
Russell Bulmer <russell.bulmer@gmail.com>
2025-12-30 00:25:16 +0000

    so slow checkpoint

--- sp_stack_render.asm
+++ sp_stack_render.asm
@@ -11,73 +11,65 @@ STACK_RENDER:														; 17 T (call)
 	; preserve SP
 	LD 			(STACK_POINTER_BACKUP), SP							; 20 T
 
-	; loop setup
-	LD 			B, 192												; 7 T
+; render row 0 / 191
+	; save end of screen row addr
+	LD 			HL, SCREEN_ROW_0									; 10 T
+	LD 			DE, (STACK_SCREEN_OFFSET)							; 20 T
+	ADD 		HL, DE												; 11 T
+	LD 			(STACK_SCREEN_SP), HL								; 16 T
+																		; = 57 T 
 
-; blit a row
-STACK_RENDER_ROW:
-	; screen address end of row +1 (PUSH pre drcrements)
-	LD			L, B												; 4 T
-    LD 			H, 0												; 7 T
-    ADD 		HL, HL      ; 2 byte adresses						; 11 T
-    LD 			DE, SCREEN_LUT ; DE = Base of table					; 10
-    ADD 		HL, DE      ; HL points to screen addr				; 11 T
-    
-    LD 			A, (HL)       ; Get Low Byte of address				; 7 T
-    INC 		HL													; 6 T
-    LD 			H, (HL)      ; Get High Byte of address				; 7 T
-    LD 			L, A												; 4 T
-    
-    LD 			(STACK_SCREEN_PTR), HL ; save it					; 16 T 
-																	; = 83 T to get screen address
-
-	; .scr/image address
-	LD 			(STACK_IMAGE_PTR), 									; 16 T
-																	; ??? T to get image addr
-
-
-	; actual blit...
 
 	; set SP to start of source row
-	LD 			SP, FAKE_PLACEHOLDER								; 20 T
+	LD 			HL, IMAGE_ROW_0										; 10 T
+	LD 			DE, (STACK_IMAGE_OFFSET)							; 20 T
+	ADD 		HL, DE												; 11 T
+	LD 			SP, HL												; 6 T
+																		; = 47 T
 
 	; pop from source to registers
 	POP 		AF 													; 10 T
 	POP 		BC 													; 10 T
 	POP 		DE 													; 10 T
 	POP 		HL													; 10 T
+																		; = 40 T
 
 	; flip to shadow registers
 	EXX 															; 4 T
 	EX AF															; 4 T
+																		; = 8 T
 
 	; pop from source to shadow registers
 	POP 		AF 													; 10 T
 	POP 		BC 													; 10 T
 	POP 		DE 													; 10 T
 	POP 		HL													; 10 T
+																		; = 40 T
 
-	LD  		SP, FAKE_PLACEHOLDER								; 20 T
+	; set SP to end of screen row
+	LD 			SP, (STACK_SCREEN_SP)								; 20 T
+																		; = 20 T
 
 	; push from shadow registers to screen
 	PUSH 		HL 													; 11 T
 	PUSH 		DE 													; 11 T
 	PUSH 		BC 													; 11 T
 	PUSH 		AF 													; 11 T
+																		; = 44 T
 
 	; flip back to regular registers
 	EXX 															; 4 T
 	EX AF															; 4 T
+																		; = 8 T
 
 	; push from registers to screen
 	PUSH 		HL 													; 11 T
 	PUSH 		DE 													; 11 T
 	PUSH 		BC 													; 11 T
 	PUSH 		AF 													; 11 T
+																		; = 44 T
 
-																	; 224 =  T for actual blit
-
-	DJNZ		STACK_RENDER_ROW									; 13 T (8 on last but meh)
+																	; = 308 T for one row blit
 
 
 	; restore SP
@@ -87,52 +79,16 @@ STACK_RENDER_ROW:
 	; none for now
 	RET								; STACK_RENDER					; 10 T
 
-; main overhead
-	; 17 call
-	; 20 save SP
-	; 20 restore SP
-	; 10 ret
-	; = 67 T overhead
 
-; per row all hard coded
-; (doesn't do the LUT stuff, just buffer row to screen row hard coded)
-	; 20 set SP
-	; 40 pop
-	; 8 shadow
-	; 40 pop
-	; 20 set SP
-	; 44 push
-	; 8 shadow
-	; 44 push
-	; = 224 T
-	;
-	; erm lol - that's a scanline :)
-	;
-	; floating bus racing the beam: 120 scanlines...
-	; so 120 rows of 16col stack blitting without contention / racing
-	; 15/24 blocks filled from the top
-	; 
-	; floating bus lower screen
-	; extra 96 scanlines = 216
-	; time for all of lower half to be blitted (2 blits per whole row)
-	;
-	; HALT & chase
-	; at 224 T per stack blit, we can't catch up if we render from the top
-	; but... can if we only do lower screen
 
-; per row with the extras...
-	; calc screen ptr: 83
-	; cal image ptr: ???
-	; actual blit: 224
-	; loop overhead... 13 T per DJNZ (ignore the 8 T on the last row...) 
-	; = ??? + 297 T total
-
-; total 192  with extras...
-	; ??? T per row
-	; 192 * ??? = 
-	; + 67 T main overhead
-	; + 7 setup B for loop
-	; total = ??? T
+; chasing the beam
+; (using this as we need all the scanlines chasing the beam)
+	; using HALT not floating bus
+	; and timing 8 vblank & 56 top border away
+	; then have all 192 main screen plus 56 bottom border before HALT
+	; = 248 scanlines
+	; = 55,552 t-states
+	; so double the floating bus... 
 
 ; floating bus racing the beam 
 ; (not using this as it's better for bottom half of screen)
@@ -144,19 +100,16 @@ STACK_RENDER_ROW:
 	; = 26,880 t-states clear before real racing & contention kicks in...
 	;
 
-; chasing the beam
-; (using this as we need all the scanlines chasing the beam)
-	; using HALT not floating bus
-	; and timing 8 vblank & 56 top border away
-	; then have all 192 main screen plus 56 bottom border before HALT
-	; = 248 scanlines
-	; so double the floating bus... 
-
 STACK_POINTER_BACKUP:
 	DEFW 		0
 
-STACK_SCREEN_PTR:
-	DEFW 		0
+; start of image row
+STACK_IMAGE_OFFSET:
+	DEFB		8
 
-STACK_IMAGE_PTR:
-	DEFW 		0
+; end of screen row (stack shit)
+STACK_SCREEN_OFFSET:
+	DEFB		8 + 16
+
+STACK_SCREEN_SP:
+	DEFW		0
